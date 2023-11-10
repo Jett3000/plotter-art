817931d85f9d443f25e9ac6cb6020ecd5e3a6a57
Jett Pavlica <jettpav@gmail.com>
2023-11-10 14:29:10 -0500

    bird as an object

--- night-bird/sketch.js
+++ night-bird/sketch.js
@@ -3,34 +3,52 @@ function setup() {
 }
 
 function draw() {
-  let wingStepSize = 12;
-  let stepDegrees = 45;
-  let commonPoint = createVector(width / 2, height / 2)
-  let leftWingPoints = [];
-  let rightWingPoints = [];
-  let stepVec = createVector(0, -wingStepSize);
-  let steps = 6;
-
-  let leftPoint = commonPoint.copy();
-  for (let i = 0; i <= steps; i++) {
-    leftWingPoints.push(leftPoint.copy());
-    stepVec.rotate(radians(random(-stepDegrees, 0)));
-    leftPoint.add(stepVec);
-  }
-  let rightPoint = commonPoint.copy();
-  stepVec = createVector(0, -wingStepSize);
-  for (let i = 0; i <= steps; i++) {
-    rightWingPoints.push(rightPoint.copy());
-    stepVec.rotate(radians(random(0, stepDegrees)));
-    rightPoint.add(stepVec);
+  noFill();
+  let birdCount = 1000;
+  let birds = [];
+  while (birdCount--) {
+    birds.push(new Bird())
   }
 
-  beginShape();
-  leftWingPoints.forEach(p => vertex(p.x, p.y))
-  endShape();
-  beginShape();
-  rightWingPoints.forEach(p => vertex(p.x, p.y))
-  endShape();
-
+  birds.forEach(bird => bird.show())
   noLoop();
+}
+
+class Bird {
+  constructor() {
+    // setup
+    let wingStepSize = random(10, 13);
+    let stepDegrees = random(20, 45);
+    let steps = 10;
+    this.pos = createVector(random(width), random(height));
+    // make left wing's points
+    let leftPoint = this.pos.copy();
+    let leftPoints = [];
+    let stepVec = createVector(0, -wingStepSize);
+    for (let i = 0; i < steps; i++) {
+      stepVec.rotate(radians(random(-stepDegrees, 0)));
+      leftPoint.add(stepVec);
+      leftPoints.push(leftPoint.copy())
+    }
+    // make right wing's points
+    let rightPoint = this.pos.copy();
+    let rightPoints = [];
+    stepVec = createVector(0, -wingStepSize);
+    for (let i = 0; i < steps; i++) {
+      stepVec.rotate(radians(random(0, stepDegrees)));
+      rightPoint.add(stepVec);
+      rightPoints.push(rightPoint.copy())
+    }
+
+    // stitch into bird
+    leftPoints.reverse();
+    leftPoints.push(this.pos.copy());
+    this.points = leftPoints.concat(rightPoints);
+  }
+
+  show() {
+    beginShape();
+    this.points.forEach(p => vertex(p.x, p.y));
+    endShape(OPEN);
+  }
 }
